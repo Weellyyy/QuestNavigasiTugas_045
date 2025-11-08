65b673aa5e8424014221990c5598bc82a22e1e31
Weellyyy <wildanprtm2304@gmail.com>
2025-11-08 19:25:52 +0700

    membuat object yang berisikan variabel list peserta

--- app/src/main/java/com/example/questnavigasitugas_045/DataModel.kt
+++ app/src/main/java/com/example/questnavigasitugas_045/DataModel.kt
@@ -8,4 +8,37 @@ data class Peserta(
     val role: String
 )
 
-
+object DataProvider {
+    val listPeserta = listOf(
+        Peserta(
+            nama = "Wildan Pratama",
+            tanggalLahir = "23-12-2004",
+            asal = "Bantul",
+            role = "Gold Laner"
+        ),
+        Peserta(
+            nama = "Wildan Indi",
+            tanggalLahir = "1995-04-20",
+            asal = "Bengkulu",
+            role = "EXP Laner"
+        ),
+        Peserta(
+            nama = "Harlan Fadhilah",
+            tanggalLahir = "1988-11-02",
+            asal = "Ngapak",
+            role = "Jungler"
+        ),
+        Peserta(
+            nama = "Dean Dwi Bekti",
+            tanggalLahir = "1991-01-10",
+            asal = "Ciamis",
+            role = "Midlaner"
+        ),
+        Peserta(
+            nama = "Rizky PP",
+            tanggalLahir = "1996-07-22",
+            asal = "Bantul",
+            role = "Roamer"
+        )
+    )
+}
