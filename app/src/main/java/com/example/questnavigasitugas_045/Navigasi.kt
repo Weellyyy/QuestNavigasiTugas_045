4f6074af44b30f3eccbfec3c40206ac9bedd1bbb
Weellyyy <wildanprtm2304@gmail.com>
2025-11-08 19:57:56 +0700

    mendeklarasikan enum class page

--- app/src/main/java/com/example/questnavigasitugas_045/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasitugas_045/Navigasi.kt
@@ -1,2 +1,20 @@
 package com.example.questnavigasitugas_045
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.questnavigasitugas_045.view.FormScreen
+import com.example.questnavigasitugas_045.view.HomeScreen
+import com.example.questnavigasitugas_045.view.ListScreen
+
+
+enum class Page {
+    Home,
+    List,
+    Formulir
+}
+
+
