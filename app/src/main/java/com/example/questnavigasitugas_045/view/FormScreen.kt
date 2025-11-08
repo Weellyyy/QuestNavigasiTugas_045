e9003a33424cfc020058a06d24e2a41eeedfd1f3
Weellyyy <wildanprtm2304@gmail.com>
2025-11-08 19:45:38 +0700

    membuat fungsi baru dan paramater

--- app/src/main/java/com/example/questnavigasitugas_045/view/FormScreen.kt
+++ app/src/main/java/com/example/questnavigasitugas_045/view/FormScreen.kt
@@ -1,2 +1,37 @@
 package com.example.questnavigasitugas_045.view
 
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.questnavigasitugas_045.R
+import com.example.questnavigasitugas_045.ui.theme.QuestNavigasiTugas_045Theme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormScreen(onBackToListClicked: () -> Unit = {}) {
+
+
+}
