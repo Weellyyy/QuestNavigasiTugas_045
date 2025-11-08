f6302537c5ecb807c3d33a4fc50f2c37fe75e31b
Weellyyy <wildanprtm2304@gmail.com>
2025-11-08 19:29:39 +0700

    membuat function bernama Homescreen

--- app/src/main/java/com/example/questnavigasitugas_045/view/HomeScreen.kt
+++ app/src/main/java/com/example/questnavigasitugas_045/view/HomeScreen.kt
@@ -1,2 +1,35 @@
 package com.example.questnavigasitugas_045.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.questnavigasitugas_045.R
+import com.example.questnavigasitugas_045.ui.theme.QuestNavigasiTugas_045Theme
+
+@Composable
+fun HomeScreen(onLoginClicked: () -> Unit = {}) {
+    
+}
+
+
+
