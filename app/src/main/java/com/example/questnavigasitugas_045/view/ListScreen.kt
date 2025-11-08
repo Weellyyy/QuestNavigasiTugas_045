d5eba6d89a6a2b120bcca6c408d08aa37aac5cef
Weellyyy <wildanprtm2304@gmail.com>
2025-11-08 19:37:58 +0700

    membuat fungsi ListScreen dengan parameter onBerandaClicked dan onFormulirClicked

--- app/src/main/java/com/example/questnavigasitugas_045/view/ListScreen.kt
+++ app/src/main/java/com/example/questnavigasitugas_045/view/ListScreen.kt
@@ -1,2 +1,37 @@
 package com.example.questnavigasitugas_045.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.role
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.questnavigasitugas_045.DataProvider
+import com.example.questnavigasitugas_045.R
+import com.example.questnavigasitugas_045.ui.theme.QuestNavigasiTugas_045Theme
+
+@Composable
+fun ListScreen(
+    onBerandaClicked: () -> Unit = {},
+    onFormulirClicked: () -> Unit = {}
+) {
+}
+
+
